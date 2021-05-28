0c91cbc75952d63d166601dad931beb7fc2a683e
Emile-web <emileerre@gmail.com>
2021-05-28 12:20:40 +0200

    created header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="App-Header">
+            <h1>Amazon Cart</h1>
+        </div>
+    )
+}
+
+export default Header
